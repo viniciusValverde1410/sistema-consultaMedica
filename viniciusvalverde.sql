419679224c2069fa7208044513097c1ea2a3196e
vinicius.valverde@aluno.senai.br <vinicius.valverde@aluno.senai.br>
2024-11-05 15:10:19 -0300

    Inserts pacientes

--- viniciusvalverde.sql
+++ viniciusvalverde.sql
@@ -30,3 +30,31 @@ CREATE TABLE consultas(
         FOREIGN KEY (id_medico)
         REFERENCES medicos (id_medico)
 );
+
+INSERT INTO pacientes (nome, idade, data_nascimento, email, telefone)
+VALUES ('Vinicius Valverde', 16, '2007/09/12', 'viniciusvalverde@gmail.com', '(11) 99689-1030');
+
+
+INSERT INTO pacientes (nome, idade, data_nascimento, email, telefone)
+VALUES ('Pedro Oliveira', 22, '2002/08/12', 'pedrosantos@gmail.com', '(19) 99699-2040'),
+('Giovanni Gonçalves', 14, '2010/04/12', 'giovannigoncalves@gmail.com', '(19) 99756-8961');
+
+
+INSERT INTO pacientes (nome, idade, data_nascimento, email, telefone)
+VALUES
+ ('Vitor Sampaio', 16, '2008/03/10', 'vitorsampaio@gmail.com', '(71) 99452-9631'),
+ ('Gabriela Fernanda', 17, '2007/10/16', 'gabibarbosa@gmail.com', '(19) 99254-8156'),
+ ('Julia Martins', 16, '2008/07/12', 'juliaarruda@gmail.com', '(19) 99234-3678');
+ 
+ INSERT INTO pacientes (nome, idade, data_nascimento, email, telefone)
+ VALUES
+ ('Vitor Argeri', 17, '2007/05/14', 'vitorargeri@gmail.com', '(19) 99169-9863'),
+ ('Mariana Silva', 16, '2008/03/12', 'marianasilva@gmail.com', '(19) 99315-7012'),
+ ('Marcos Junior' , 17, '2007/05/14', 'marcosjunior@gmail.com', '(19) 99041-0513'),
+ ('João Pedro' , 17, '2007/01/27', 'joaopedro@gmail.com', '(19) 99879-1034');
+ 
+ INSERT INTO pacientes (nome, idade, data_nascimento, email, telefone)
+ VALUES
+ ('Miguel Martins', 16, '2008/07/28', 'miguelsilva@gmail.com', '(19) 99629-1932');
+
+ 
